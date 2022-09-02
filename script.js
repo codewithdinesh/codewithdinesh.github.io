59c560d57abef34c0f09b05dc3826c1c5dc6fc92
Dinesh Rathod <pr760677@gmail.com>
2022-09-03 03:27:22 +0530

    JQuery removed from project and devloper title typer created

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
+// nav toggle button
+const navToggle = document.getElementById("nav-toggle");
 
-<<<<<<< HEAD
-(function ($) { // Begin jQuery
-  $(function () { // DOM ready
+// Nav list 
+const navlist = document.getElementById("nav-list");
+
+// Nav items
+const navItems = document.getElementsByClassName("nav-item");
+
+// developer title
+const devTitle = document.getElementById("devTitle")
+
+const onNavToggle = () => {
+
+  navlist.classList.toggle("active");
+  navToggle.classList.toggle("active");
+
+  // all elements are visible now
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].classList.toggle("active");
+  }
+
+};
+
+// open and close navigation button and navigation
+navToggle.addEventListener("click", () => {
+  onNavToggle();
+});
+
+const devTitles = [
+  "Full stack Android Devloper",
+  "Tech enthusiast",
+  "Full stack Web Devloper",
+  "Coding Enthusiast"
+]
+
+// devloper Title Text changes 
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const typer = (el) => {
+  const texts = JSON.parse(el.dataset.rotate);
+  const period = +el.dataset.period || 1000;
+  const speed = +el.dataset.speed || 200;
+  const textsTot = texts.length;
+
+  let tx = 0;
+  let ch = 0;
+  let chTot = 0;
+  let text = ""
+
+  const typeIt = () => {
+    if (ch > chTot) return setTimeout(typeText, period);
+    el.textContent = text.substring(0, ch++);
+    setTimeout(typeIt, rand(Math.min(60, speed - 80), speed + 80));
+  };
 
 
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function () {
-      $('#nav-list').slideToggle();
-    });
-    // Hamburger to X toggle
-    $('#nav-toggle').on('click', function () {
-      this.classList.toggle('active');
-    });
-  }); // end DOM ready
-})(jQuery); // end jQuery
-=======
+  const typeText = () => {
+    ch = 0;
+    text = texts[tx];
+    chTot = text.length;
 
+    typeIt();
 
-(function($) { // Begin jQuery
-    $(function() { // DOM ready
-     
+    tx += 1;
+    tx %= texts.length;
+  };
 
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); // end DOM ready
-  })(jQuery); // end jQuery
->>>>>>> 66198f5864bed4b549e2cbf0300fee81365de9c8
+  typeText();
+
+};
+
+document.querySelectorAll("[data-rotate]").forEach(typer);
+
